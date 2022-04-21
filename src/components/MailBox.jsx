a77d3356503c97177bd330eeeca6528d51fb6549
Nanda kishor palei <nandakishorpalei7676@gmail.com>
2022-04-21 19:44:36 +0530

    Update MailBox.jsx

--- src/components/MailBox.jsx
+++ src/components/MailBox.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import AllMail from './mailBody/AllMail';
-import Menubar from './menubar/Menubar';
-import Navbar from './navbar/Navbar';
-import { Routes, Route } from 'react-router-dom';
-import SelectedMail from "./selectedMail/SelectedMail"
+import React from "react";
+import AllMail from "./mailBody/AllMail";
+import Menubar from "./menubar/Menubar";
+import Navbar from "./navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import SelectedMail from "./selectedMail/SelectedMail";
 
 const MailBox = () => {
-    return (
-        <div>
-        <Navbar />
-        <div style={{display:'flex', justifyContent:"space-between", width: "98vw"}}>
-            <Menubar />
-            <Routes>
-              <Route path="/" element={<AllMail />}></Route>
-              <Route path="/query" element={<SelectedMail />}></Route>
-            </Routes>
-        </div>
-        </div>
-    );
+  return (
+    <div> 
+      <Navbar />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "98vw",
+        }}
+      >
+        <Menubar />
+        <Routes>
+          <Route path="/tag" element={<AllMail /> }></Route>
+          <Route path="/" element={<AllMail />}></Route>
+          <Route path="/query" element={<SelectedMail />}></Route>
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default MailBox;
