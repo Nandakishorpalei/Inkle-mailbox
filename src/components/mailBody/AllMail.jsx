f4c0e0edc1d303736196c500365d4794e4a6f447
Nanda kishor palei <nandakishorpalei7676@gmail.com>
2022-04-21 19:41:13 +0530

    Update AllMail.jsx

--- src/components/mailBody/AllMail.jsx
+++ src/components/mailBody/AllMail.jsx
@@ -1,38 +1,54 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { getData } from '../mailRedux/action';
-import "./allMail.css"
+import React, { useEffect, useLayoutEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useSearchParams, Link } from "react-router-dom"; 
+import { filterByCategory, getData } from "../mailRedux/action";
+import "./allMail.css";
 
 const AllMail = () => {
+  const dispatch = useDispatch();
+  const [params, setParams] = useSearchParams();
+  const tagName = params.get("name"); 
 
-    const dispatch = useDispatch();
-    const {loading, data} = useSelector((store) => store.mailData)
-    console.log('data:', data)
 
-    useEffect(()=>{
-     fetchData(dispatch)()
-    },[])
+  const { loading, data } = useSelector((store) => store.mailData);
 
-    function fetchData(dispatch){
-        return ()=>{
-            dispatch(getData(dispatch))
-        }
+  useLayoutEffect(()=>{
+    if(tagName){
+      dispatch(filterByCategory(tagName));
     }
-    return loading ? <div style={{textAlign:"center", width: "78vw"}}><progress></progress></div> : (
-        <div id="mailBody" style={{width: "80vw", textAlign:"left"}}>
-            {data.map((mail)=>{
-                return (
-                    <div className="mailHeader" key={mail.id}>
-                    <Link to={`/query?id=${mail.id}`} style={{textDecoration:"none",color:"black"}}>
-                <span className='mailSubject'>{mail.subject} </span>
-                - {mail.body}
-                </Link>
-                </div>
-                )
-            })}
-        </div>
-    );
+
+  },[])
+
+  useEffect(() => {
+    fetchData(dispatch)();
+  }, []);
+
+  function fetchData(dispatch) {
+    return () => {
+      dispatch(getData(dispatch));
+    };
+  }
+  return loading ? (
+    <div style={{ textAlign: "center", width: "78vw" }}>
+      <progress></progress>
+    </div>
+  ) : (
+    <div id="mailBody" style={{ width: "80vw", textAlign: "left" }}>
+      {data.map((mail) => {
+        return (
+          <div className="mailHeader" key={mail.id}>
+            <Link
+              to={`/query?id=${mail.id}`}
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              <h3 className="mailSubject">{mail.subject} </h3>
+              <h5 className="mailBodyContent">{mail.body}</h5>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default AllMail;
